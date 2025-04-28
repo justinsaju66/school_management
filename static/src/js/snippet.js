e5019d659df83e97304c97bf84c755e6180c1285
Justin <justinsaju66@gmail.com>
2025-04-28 12:53:32 +0530

    some changes

--- static/src/js/snippet.js
+++ static/src/js/snippet.js
@@ -1,9 +1,8 @@
+
 /** @odoo-module */
 import { renderToElement } from "@web/core/utils/render";
 import publicWidget from "@web/legacy/js/public/public_widget";
 import { rpc } from "@web/core/network/rpc";
-
-var core = require('web.core');
 var qweb = core.qweb;
 var Dynamic = PublicWidget.Widget.extend({
 
@@ -31,14 +30,7 @@ var chunks = _.chunk(this.data, 4)
 
 chunks[0].is_active = true
 
-this.$el.find('#courosel').html(
-
-qweb.render('school_management.event_data', {
-
-chunks
-
-})
-)
+this.$el.find('#courosel').html(qweb.render('school_management.event_data', {chunks}))
 },
 
 PublicWidget.registry.dynamic_snippet_blog = Dynamic;
